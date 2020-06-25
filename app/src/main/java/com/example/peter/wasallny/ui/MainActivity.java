76009c76e49828cbf133468b0c7ab330ea537082
lPROTONl <37454592+lPROTONl@users.noreply.github.com>
2020-06-25 10:11:11 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/peter/wasallny/ui/MainActivity.java
+++ app/src/main/java/com/example/peter/wasallny/ui/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         connectivity= (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 
-        
+        getSharedPreferences();
 
         manager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {

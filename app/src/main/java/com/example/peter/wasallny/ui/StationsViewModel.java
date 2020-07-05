7881d522b1be985502c4e002c7089cc17bf38d96
lPROTONl <37454592+lPROTONl@users.noreply.github.com>
2020-07-05 15:31:24 +0200

    Adjust Variables Names

--- app/src/main/java/com/example/peter/wasallny/ui/StationsViewModel.java
+++ app/src/main/java/com/example/peter/wasallny/ui/StationsViewModel.java
@@ -25,8 +25,8 @@ public class StationsViewModel extends ViewModel {
     List<String> line3 = Arrays.asList("El Shams Club", "Alf Maskan", "Heliopolis", "Haroun", "Al Ahram", "Koleyet El Banat", "Stadium", "Fair Zone", "Abbassiya", "Abdou Pasha", "El Geish", "Bab El Shaaria", "Attaba");
 
     List<String> stations = Arrays.asList("New El Marg",     "El Marg",     "Ezbet El Nakhl",     "Ain Shams",     "El Matareyya",     "Helmeyet El Zaitoun",     "Hadayeq El Zaitoun",     "Saray El Qobba",     "Hammamat El Qobba",      "Kobri El Qobba",       "Manshiet El Sadr",      "El Demerdash",       "Ghamra",       "Al Shohadaa",       "Orabi",       "Nasser",       "Sadat",       "Saad Zaghloul",       "Al Sayeda Zeinab",       "El Malek El Saleh",      "Mar Girgis",      "El Zahraa",      "Dar El Salam",       "Hadayek El Maadi",      "Maadi",      "Sakanat El Maadi",      "Tora El Balad",      "Kozzika",       "Tora El Asmant",      "El Maasara",      "Hadayek Helwan",     "Wadi Hof",      "Helwan University",      "Ain Helwan",       "Helwan",      "Shubra El Kheima",      "Kolleyyet El Zeraa",       "Mezallat",      "Khalafawy",      "St. Teresa",       "Rod El Farag",      "Masarra",      "Attaba",      "Mohamed Naguib",      "Opera",      "Dokki",     "El Bohoth",      "Cairo University",      "Faisal",      "Giza",      "Omm El Masryeen",      "Sakiat Mekky",      "El Mounib",      "El Shams Club",      "Alf Maskan",      "Heliopolis",       "Haroun",      "Al Ahram",      "Koleyet El Banat",      "Stadium",      "Fair Zone",       "Abbassiya",       "Abdou Pasha",      "El Geish",     "Bab El Shaaria");
-    List<Double> stationslat = Arrays.asList(30.163649,       30.152086,     30.139288,            30.131043,       30.121374,          30.114349,                 30.105198,                30.098078,            30.090337,                30.086967,              30.082214,               30.077256,            30.068970,      30.061848,           30.057330,     30.053555,      30.044352,     30.036560,             30.029236,                30.016876,                30.005849,         29.995243,        29.982033,            29.970105,               29.959830,    29.952835,               29.946332,            29.936170,       29.925850,             29.906112,         29.897193,            29.879440,       29.869025,                29.862657,          29.84888,      30.122483,               30.113778,                  30.105007,       30.097967,        30.088336,          30.080570,           30.071045,      30.052418,     30.045411,             30.042063,    30.038231,   30.035756,        30.025994,               30.017281,     30.010606,   30.005194,              29.995470,           29.981332,        30.125034,            30.118770,         30.108072,          30.102088,     30.091293,       30.083676,               30.072948,      30.073319,         30.072786,         30.065437,          30.062275,      30.055129);
-    List<Double> stationslong = Arrays.asList(31.338364,      31.335757,     31.324497,            31.319073,       31.313977,          31.313891,                 31.309932,                31.304750,            31.298114,                31.293823,              31.287842,               31.277810,            31.264678,      31.246112,           31.242534,     31.238800,      31.235597,     31.238086,             31.235388,                31.230963,                31.229525,         31.231526,        31.242201,            31.250661,               31.258069,    31.263471,               31.273582,            31.281736,       31.287696,             31.299729,         31.304069,            31.313408,       31.320371,                31.325012,          31.334206,     31.244610,               31.248660,                  31.246739,       31.245393,        31.245457,          31.245425,           31.245033,      31.246863,     31.244255,             31.225169,    31.211999,   31.200198,        31.201190,                31.204001,    31.207091,   31.208008,              31.208620,           31.211822,        31.348327,            31.340183,         31.338235,          31.333691,     31.326572,       31.328815,               31.317390,      31.301151,         31.284093,         31.276942,          31.267956,      31.257608);
+    List<Double> stationsLat = Arrays.asList(30.163649,       30.152086,     30.139288,            30.131043,       30.121374,          30.114349,                 30.105198,                30.098078,            30.090337,                30.086967,              30.082214,               30.077256,            30.068970,      30.061848,           30.057330,     30.053555,      30.044352,     30.036560,             30.029236,                30.016876,                30.005849,         29.995243,        29.982033,            29.970105,               29.959830,    29.952835,               29.946332,            29.936170,       29.925850,             29.906112,         29.897193,            29.879440,       29.869025,                29.862657,          29.84888,      30.122483,               30.113778,                  30.105007,       30.097967,        30.088336,          30.080570,           30.071045,      30.052418,     30.045411,             30.042063,    30.038231,   30.035756,        30.025994,               30.017281,     30.010606,   30.005194,              29.995470,           29.981332,        30.125034,            30.118770,         30.108072,          30.102088,     30.091293,       30.083676,               30.072948,      30.073319,         30.072786,         30.065437,          30.062275,      30.055129);
+    List<Double> stationsLong = Arrays.asList(31.338364,      31.335757,     31.324497,            31.319073,       31.313977,          31.313891,                 31.309932,                31.304750,            31.298114,                31.293823,              31.287842,               31.277810,            31.264678,      31.246112,           31.242534,     31.238800,      31.235597,     31.238086,             31.235388,                31.230963,                31.229525,         31.231526,        31.242201,            31.250661,               31.258069,    31.263471,               31.273582,            31.281736,       31.287696,             31.299729,         31.304069,            31.313408,       31.320371,                31.325012,          31.334206,     31.244610,               31.248660,                  31.246739,       31.245393,        31.245457,          31.245425,           31.245033,      31.246863,     31.244255,             31.225169,    31.211999,   31.200198,        31.201190,                31.204001,    31.207091,   31.208008,              31.208620,           31.211822,        31.348327,            31.340183,         31.338235,          31.333691,     31.326572,       31.328815,               31.317390,      31.301151,         31.284093,         31.276942,          31.267956,      31.257608);
 
     ArrayList<String> sub = new ArrayList<>();
     ArrayList<String> sub2 = new ArrayList<>();
@@ -38,7 +38,7 @@ public class StationsViewModel extends ViewModel {
 
     Location startPoint;
     Location endPoint;
-    ArrayList<Float> distancelist = new ArrayList<>();
+    ArrayList<Float> distanceList = new ArrayList<>();
 
     static MutableLiveData<ArrayList<String>> stationsNames = new MutableLiveData<>();
 
@@ -52,7 +52,7 @@ public class StationsViewModel extends ViewModel {
                  Toast.makeText(MainActivity.context, "Check your Internet Connetion", Toast.LENGTH_SHORT).show();
                  return;
              } else {
-                 Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + stationslat.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString())) + "," + stationslong.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString()))));
+                 Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + stationsLat.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString())) + "," + stationsLong.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString()))));
                  MainActivity.context.startActivity(in);
              }
          } else if (Build.VERSION.SDK_INT < 23) {
@@ -63,7 +63,7 @@ public class StationsViewModel extends ViewModel {
                  Toast.makeText(MainActivity.context, "Check your Internet Connetion", Toast.LENGTH_SHORT).show();
                  return;
              } else {
-                 Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + stationslat.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString())) + "," + stationslong.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString()))));
+                 Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + stationsLat.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString())) + "," + stationsLong.get(stations.indexOf(MetroLinesFragment.binding.toText.getText().toString()))));
                  MainActivity.context.startActivity(in);
              }
 
@@ -79,7 +79,7 @@ public class StationsViewModel extends ViewModel {
                 return;
             } else {
                 try {
-                    distancelist.clear();
+                    distanceList.clear();
                     startPoint = new Location("locationA");
                     startPoint.setLatitude(MainActivity.latitude);
                     startPoint.setLongitude(MainActivity.longitude);
@@ -88,16 +88,16 @@ public class StationsViewModel extends ViewModel {
                     endPoint = new Location("locationA");
                     for (int i = 0; i < stations.size(); i++) {
                         if (i < stations.size()) {
-                            endPoint.setLatitude(stationslat.get(i));
-                            endPoint.setLongitude(stationslong.get(i));
+                            endPoint.setLatitude(stationsLat.get(i));
+                            endPoint.setLongitude(stationsLong.get(i));
                             distance = (startPoint.distanceTo(endPoint)) / 1000;
-                            distancelist.add(distance);
+                            distanceList.add(distance);
                         } else {
                             break;
                         }
-                        Float min = Collections.min(distancelist);
+                        Float min = Collections.min(distanceList);
 
-                        MetroLinesFragment.binding.nearText.setText("Nearest Station to you is " + stations.get(distancelist.indexOf(min)));
+                        MetroLinesFragment.binding.nearText.setText("Nearest Station to you is " + stations.get(distanceList.indexOf(min)));
 
                     }
                 } catch (Exception e) {
@@ -112,7 +112,7 @@ public class StationsViewModel extends ViewModel {
                 return;
             } else {
                 try {
-                    distancelist.clear();
+                    distanceList.clear();
                     startPoint = new Location("locationA");
                     startPoint.setLatitude(MainActivity.latitude);
                     startPoint.setLongitude(MainActivity.longitude);
@@ -121,16 +121,16 @@ public class StationsViewModel extends ViewModel {
                     endPoint = new Location("locationA");
                     for (int i = 0; i < stations.size(); i++) {
                         if (i < stations.size()) {
-                            endPoint.setLatitude(stationslat.get(i));
-                            endPoint.setLongitude(stationslong.get(i));
+                            endPoint.setLatitude(stationsLat.get(i));
+                            endPoint.setLongitude(stationsLong.get(i));
                             distance = (startPoint.distanceTo(endPoint)) / 1000;
-                            distancelist.add(distance);
+                            distanceList.add(distance);
                         } else {
                             break;
                         }
-                        Float min = Collections.min(distancelist);
+                        Float min = Collections.min(distanceList);
 
-                        MetroLinesFragment.binding.nearText.setText("Nearest Station to you is " + stations.get(distancelist.indexOf(min)));
+                        MetroLinesFragment.binding.nearText.setText("Nearest Station to you is " + stations.get(distanceList.indexOf(min)));
 
                     }
                 } catch (Exception e) {
